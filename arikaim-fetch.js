4553df906b8567533fbc3e394f53f6bae54efb39
Arikaim <info@arikaim.com>
2024-08-05 19:55:58 +0300

    =

--- arikaim-fetch.js
+++ arikaim-fetch.js
@@ -9,14 +9,28 @@
 function ArikaimClient(url, key) {
     var endpoint = url;
     var apiKey = key;
-    var version = '1.0.0';
+    var version = '1.0.1';
     var self = this;
-    
-    var headers = {
-        'Content-Type': 'application/json',
-        'Authorization' : apiKey
+    var options = {      
+        credentials: 'include',
+        mode: 'cors',
+        headers: headers 
     };
+    var headers = {}
 
+    headers['Content-Type'] = 'application/json';
+    if (isEmpty(apiKey) == false) {
+        headers['Authorization'] = apiKey;
+    }
+      
+    this.getEndpoint = function() {
+        if (isEmpty(endpoint) == true) {
+            return '';
+        }
+
+        return endpoint;
+    };
+    
     this.get = function(url) {
         return this.request('POST',url);         
     };
@@ -34,9 +48,8 @@ function ArikaimClient(url, key) {
     };
 
     this.request = function(method, url, data) {
-        return new Promise(function(resolve, reject) {
-            console.log(endpoint + url);
-            fetch(endpoint + url,self.getOptions(method,data))
+        return new Promise(function(resolve, reject) {          
+            fetch(self.getEndpoint() + url,self.getOptions(method,data))
                 .then(async function(response) {
                     var json = await response.json();
                     return (response.ok == true) ? resolve(json) : reject(json);   
@@ -47,13 +60,12 @@ function ArikaimClient(url, key) {
         });  
     };
 
-    this.getOptions = function(method, data) {
-        var options = {
-            method: method,
-            mode: 'cors',
-            headers: headers 
-        }
+    this.setOptions = function(requestOptions) {
+        options = requestOptions;
+    };
 
+    this.getOptions = function(method, data) {       
+        options['method'] = method;
         if (typeof data === 'object') {
             options['body'] = JSON.stringify(data);
         }
